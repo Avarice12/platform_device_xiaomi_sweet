27f597d05362587b6fec6bf656ae54d2714f5fb8
Rafiester <rafi.haqqi@gmail.com>
2023-12-15 23:42:26 +0700

    sweet: Evolution-X Bringup
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -11,11 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit common Evolution-X Stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_sweet
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
